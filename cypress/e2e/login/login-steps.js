b91c1a453c8c7250c5a36f087abfdad99314be25
Scott MacLeod <scott@macleod.tech>
2022-08-30 21:35:25 +0100

    feat: Use password as a secret from Github secrets

--- cypress/e2e/login/login-steps.js
+++ cypress/e2e/login/login-steps.js
@@ -2,6 +2,8 @@ import { Before, Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 const url = "https://www.saucedemo.com/";
 
+const password = Cypress.env('user_password')
+
 Given('I open the Swag Labs website', () => {
   cy.visit(url);
 });
@@ -9,7 +11,7 @@ Given('I open the Swag Labs website', () => {
 
 When('I submit valid credentials into the login form', () => {
     cy.get('#user-name').click().type('standard_user')
-    cy.get('#password').click().type('secret_sauce')
+    cy.get('#password').click().type(password)
     cy.get('#login-button').click()
 })
 
@@ -38,7 +40,7 @@ Then('I am shown an error to say my credentials are incorrect', () => {
 
 When('I submit valid credentials for a locked out user into the login form', () => {
   cy.get('#user-name').click().type('locked_out_user')
-  cy.get('#password').click().type('secret_sauce')
+  cy.get('#password').click().type(password)
   cy.get('#login-button').click()
 })
 
